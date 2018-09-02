13eeefe7ea719e4506d26ecc7c889d0cf24c5dd0
Marcos Soares <mavmaso@gmail.com>
2018-09-02 16:39:51 -0300

    Improving rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@ require 'simplecov'
 SimpleCov.start
 
 RSpec.configure do |config|
+  config.order = :random
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
