fb5bddf70cc6d07c653e2ff95f5afc0b0c1fe469
Marcos Soares <mavmaso@gmail.com>
2018-09-02 22:43:45 -0300

    Adding show action to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::UsersController < ApplicationController
+  respond_to :json
+
   def show
-    
+    begin
+      set_user
+      respond_with @user
+    rescue => exception
+      head 404
+    end
+  end
+
+  private 
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -20,7 +20,19 @@ RSpec.describe 'Users Api', type: :request do
     context 'when the user exists' do
       it 'returns the user' do
         user_response = JSON.parse(response.body)
-        expect(user_response['id'].to eq(user_id))
+        expect(user_response['id']).to eq(user_id)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the user does not exist' do
+      let(:user_id) { 1000 }
+      
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
       end
     end
   end
